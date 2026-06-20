8b7c486d7c6e5ff5b30ae03bd55b3cdeb14518fc
Sergio <your_email@example.com>
2026-06-21 00:36:55 +0300

    add conv res

--- src/main/java/com/mshr/model/ConversionResult.java
+++ src/main/java/com/mshr/model/ConversionResult.java
@@ -14,5 +14,31 @@ public class ConversionResult {
         amount, fromCurrency, convertedAmount, toCurrency, date);
   }
 
-  ConversionResult(double amount, String fromCurrency, String toCurrency, double convertedAmount, String date);
+  ConversionResult(double amount, String fromCurrency, String toCurrency, double convertedAmount, String date) {
+    this.amount = amount;
+    this.fromCurrency = fromCurrency;
+    this.toCurrency = toCurrency;
+    this.convertedAmount = convertedAmount;
+    this.date = date;
+  };
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public String getFromCurrency() {
+    return fromCurrency;
+  }
+
+  public String getToCurrency() {
+    return toCurrency;
+  }
+
+  public double getConvertedAmount() {
+    return convertedAmount;
+  }
+
+  public String getDate() {
+    return date;
+  }
 };
